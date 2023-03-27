c535a416e010cba3d96750f355c94f51c5ed8261
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2023-03-27 22:51:23 +0300

    feat: add styles card

---
+++ js/star.js
@@ -0,0 +1,12 @@
+const stars = document.querySelectorAll(".stars i");
+const starsNone = document.querySelector(".hero-star");
+
+// ---- ---- Stars ---- ---- //
+stars.forEach((star, index1) => {
+	star.addEventListener("click", () => {
+		stars.forEach((star, index2) => {
+			// ---- ---- Active Star ---- ---- //
+			index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+		});
+	});
+});
