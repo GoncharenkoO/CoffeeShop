fda7b3d82d4b8b6975105411ab50fe6b587706a5
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2023-04-02 23:16:52 +0300

    styles slider

--- js/swiper.js
+++ js/swiper.js
@@ -4,22 +4,40 @@ let mySwiper = new Swiper(slider, {
 	slidesPerView: 3,
 	spaceBetween: 10,
 	centeredSlides: true,
-	loop: true,
+	// loop: true,
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
 		dynamicBullets: true,
 	},
-	grabCursor: true,
+	// grabCursor: true,
 	mousewheel: true,
 	keyboard: {
 		enabled: true,
 		onlyInViewport: true,
 		pageUpDown: true,
-  },
-  autoHeight: true,
+	},
+	// autoHeight: true,
+	watchOverflow: true,
 	// navigation: {
 	// 	nextEl: '.swiper-button-next',
 	// 	prevEl: '.swiper-button-prev',
 	// },
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		// when window width is >= 480px
+		480: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 4,
+			spaceBetween: 40,
+		},
+	},
 });
